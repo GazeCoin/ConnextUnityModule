7950d8b11d88dee0ca3b4da55ecf680cef895886
Nick Taras <nicktaras@hotmail.co.uk>
2019-06-05 22:17:54 +1000

    split classes per type accounts, payments, transaction

---
+++ eth_unity_project/Assets/AccountModuleScripts/Balance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using Nethereum.JsonRpc.UnityClient;
+using UnityEngine;
+
+public class Balance {
+    public IEnumerator GetBalance(string server, string address, System.Action<decimal> callback)
+    {
+        var getBalanceRequest = new EthGetBalanceUnityRequest(server);
+        yield return getBalanceRequest.SendRequest(address, Nethereum.RPC.Eth.DTOs.BlockParameter.CreateLatest());
+        if (getBalanceRequest.Exception == null)
+        {
+            var balance = getBalanceRequest.Result.Value;
+            callback(Nethereum.Util.UnitConversion.Convert.FromWei(balance, 18));
+        }
+        else
+        {
+            throw new System.InvalidOperationException("Get balance request failed");
+        }
+    }
+}
---
+++ eth_unity_project/Assets/AccountModuleScripts/BlockChainController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockChainController : MonoBehaviour {
+	void Start () {
+
+        Payment _payment = new Payment();
+        Balance _balance = new Balance();
+        Account _account = new Account();
+
+        // Payment example
+        StartCoroutine(_payment.MakePayment(
+           "HTTP://127.0.0.1:7545", // server
+           "750f470f331da664f26b3ca8e05f30b54e21abf0bdbb4706a1ce920c4fd147aa", // from priv
+           "0x179496CeA107ee91e8738724CE2931924A65E4eD", // from pub
+           "0x7B3150cC598FD65058044Dec34c9db545b8E1E90", // to
+           1.1m, // send amount
+           2 // gas
+        ));
+
+        // Balance example
+        StartCoroutine(_balance.GetBalance(
+            "HTTP://127.0.0.1:7545", 
+            "0x179496CeA107ee91e8738724CE2931924A65E4eD", (balance) => {
+            Debug.Log(balance);
+        }));
+
+        // Create account example
+        _account.CreateAccount("strong_password", (address, encryptedJson) => {
+            Debug.Log(address);
+            Debug.Log(encryptedJson);
+        });
+
+	}
+}
